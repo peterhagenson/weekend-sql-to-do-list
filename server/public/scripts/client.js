7837f62bc29b91bff7a2b7a4b34df0ff2055f45d
Peter Hagenson <p.hagenson@gmail.com>
2022-08-05 05:03:37 -0500

    P

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,7 +3,33 @@ console.log("js");
 $(document).ready(onReady);
 
 function onReady() {
+  $("#createBtn").on("click", handleCreate);
   getTasks();
+}
+
+function handleCreate() {
+  console.log("clicked");
+  let taskToSend = {};
+  taskToSend.task = $("#taskIn").val();
+  taskToSend.status = "incomplete";
+  console.log(taskToSend);
+  addTask(taskToSend);
+}
+
+function addTask(task) {
+  $.ajax({
+    method: "POST",
+    url: "/todo",
+    data: task,
+  })
+    .then(function (response) {
+      console.log("response from server:", response);
+      getTasks();
+    })
+    .catch(function (error) {
+      console.log("error in POST", error);
+      alert("unable to add task");
+    });
 }
 
 function getTasks() {
@@ -11,10 +37,15 @@ function getTasks() {
   $.ajax({
     method: "GET",
     url: "/todo",
-  }).then(function (response) {
-    console.log(response);
-    renderTasks(response);
-  });
+  })
+    .then(function (response) {
+      console.log(response);
+      renderTasks(response);
+    })
+    .catch(function (error) {
+      console.log("error in GET", error);
+      alert("unable to add task");
+    });
   // .catch(function (error) {
   //   console.log("error in getTasks");
   // });
--- server/routes/weekend-to-do-list.router.js
+++ server/routes/weekend-to-do-list.router.js
@@ -4,18 +4,22 @@ const router = express.Router();
 
 const pool = require("../modules/pool");
 
-//const pg = require("pg");
+router.post("/", (req, res) => {
+  let newTask = req.body;
+  console.log("Adding task", newTask);
 
-//const Pool = pg.Pool;
-
-// Pool (caps P) and "new" are a constructor
-// const pool = new Pool({
-//   database: "weekend-to-do-list", //this can and will change for each project
-//   host: "localhost",
-//   PORT: 5432,
-//   max: 10, // limits how many people can use your database at a time
-//   idleTimeoutMillis: 30000, // how long to wait to hang up phone on someone
-// });
+  let queryText = `INSERT INTO "weekend-to-do-app" ("task", "status")
+                    VALUES ($1, $2);`;
+  pool
+    .query(queryText, [newTask.task, newTask.status])
+    .then((result) => {
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log("error adding new book", error);
+      res.sendStatus(500);
+    });
+});
 
 router.get("/", (req, res) => {
   console.log("in /todo GET");
