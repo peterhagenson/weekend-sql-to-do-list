0b122b0399693ffaf411927c653bef90f6f2ab14
Peter Hagenson <p.hagenson@gmail.com>
2022-08-04 20:25:09 -0500

    established GET route and tested connection to database

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,34 @@
 console.log("js");
+
+$(document).ready(onReady);
+
+function onReady() {
+  getTasks();
+}
+
+function getTasks() {
+  console.log("in getTasks");
+  $.ajax({
+    method: "GET",
+    url: "/todo",
+  }).then(function (response) {
+    console.log(response);
+    renderTasks(response);
+  });
+  // .catch(function (error) {
+  //   console.log("error in getTasks");
+  // });
+}
+
+function renderTasks(listOfTasks) {
+  $("#displayContainer").empty();
+
+  for (let task of listOfTasks) {
+    $("#displayContainer").append(`
+        <tr>
+            <td>${task.task}</td>
+            <td>${task.status}</td>
+        <tr>
+        `);
+  }
+}
--- server/routes/weekend-to-do-list.router.js
+++ server/routes/weekend-to-do-list.router.js
@@ -1,17 +1,35 @@
 const express = require("express");
+//const bodyParser = require("body-parser");
 const router = express.Router();
 
-const pg = require("pg");
+const pool = require("../modules/pool");
 
-const Pool = pg.Pool;
+//const pg = require("pg");
+
+//const Pool = pg.Pool;
 
 // Pool (caps P) and "new" are a constructor
-const pool = new Pool({
-  database: "music_library", //this can and will change for each project
-  host: "localhost",
-  PORT: 5432,
-  max: 10, // limits how many people can use your database at a time
-  idleTimeoutMillis: 30000, // how long to wait to hang up phone on someone
+// const pool = new Pool({
+//   database: "weekend-to-do-list", //this can and will change for each project
+//   host: "localhost",
+//   PORT: 5432,
+//   max: 10, // limits how many people can use your database at a time
+//   idleTimeoutMillis: 30000, // how long to wait to hang up phone on someone
+// });
+
+router.get("/", (req, res) => {
+  console.log("in /todo GET");
+  let queryText = `SELECT * FROM "weekend-to-do-app";`;
+  pool
+    .query(queryText)
+    .then((result) => {
+      console.log(result);
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log(`error making query ${queryText}, err`);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
