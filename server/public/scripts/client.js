48d55cb8a21e03d689ebf0161edec7cfa1455c12
Peter Hagenson <p.hagenson@gmail.com>
2022-08-07 15:22:42 -0500

    added more bootstrap styling

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -96,7 +96,7 @@ function renderTasks(listOfTasks) {
     if (!task.complete) {
       $("#displayContainer").append(`
         <tr class="todoRow" data-id=${task.id}>
-            <td width ="65%">${task.task}</td>
+            <td class="textNotComplete" width ="65%">${task.task}</td>
             <td><button id="completeBtn" class="btn btn-success">COMPLETE</button></td>
             <td><button id="deleteBtn" class="btn btn-danger">DELETE</button></td>
         </tr>
@@ -104,7 +104,7 @@ function renderTasks(listOfTasks) {
     } else {
       $("#displayContainer").append(`
         <tr class="todoRow" id="taskComplete" data-id=${task.id}>
-            <td width="66%">${task.task}</td>
+            <td class="textComplete" width="66%">${task.task}</td>
             <td width="17%" class="check">✓</td>
             <td width="17%"><button id="deleteBtn" class="btn btn-danger">DELETE</button></td>
         </tr>
